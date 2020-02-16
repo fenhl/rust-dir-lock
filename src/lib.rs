c041f91ff3a10aeedad40774b5386088a6816f4f
Fenhl <fenhl@fenhl.net>
2020-02-16 10:32:36 +0000

    Add new_sync method

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use {
         fs::File as SyncFile,
         io::{
             self,
+            Read as _,
             Write as _
         },
         mem::forget,
@@ -13,6 +14,7 @@ use {
             Path,
             PathBuf
         },
+        thread,
         time::Duration
     },
     async_std::{
@@ -64,7 +66,7 @@ impl<T, E: IoResultExt> IoResultExt for Result<T, E> {
 }
 
 impl DirLock<'_> {
-    pub async fn new<P: AsRef<Path>>(path: &P) -> Result<DirLock<'_>, Error> {
+    pub async fn new(path: &impl AsRef<Path>) -> Result<DirLock<'_>, Error> {
         let path = path.as_ref();
         loop {
             match fs::create_dir(path).await { // see https://github.com/rust-lang/rustup.rs/issues/988
@@ -99,6 +101,42 @@ impl DirLock<'_> {
         }
     }
 
+    /// Blocks the current thread until the lock can be established.
+    pub fn new_sync(path: &impl AsRef<Path>) -> Result<DirLock<'_>, Error> {
+        let path = path.as_ref();
+        loop {
+            match std::fs::create_dir(path) { // see https://github.com/rust-lang/rustup.rs/issues/988
+                Ok(()) => {
+                    let pidfile = path.join("pid");
+                    writeln!(SyncFile::create(&pidfile).at(&pidfile)?, "{}", std::process::id()).at(pidfile)?;
+                    return Ok(DirLock(path));
+                }
+                Err(e) => match e.kind() {
+                    io::ErrorKind::AlreadyExists => {
+                        if match SyncFile::open(path.join("pid")) {
+                            Ok(mut f) => {
+                                let mut buf = String::default();
+                                f.read_to_string(&mut buf).at(path.join("pid"))?;
+                                !buf.is_empty() // assume pidfile is still being written if empty //TODO check timestamp
+                                && !block_on(pid_exists(buf.trim().parse()?))?
+                            }
+                            Err(e) => if e.kind() == io::ErrorKind::NotFound {
+                                false
+                            } else {
+                                return Err(e.at(path.join("pid")));
+                            }
+                        } {
+                            DirLock(path).clean_up_sync()?;
+                        }
+                        thread::sleep(Duration::from_secs(1));
+                        continue;
+                    }
+                    _ => { return Err(e.at(path)); }
+                }
+            }
+        }
+    }
+
     pub async fn drop_async(self) -> Result<(), Error> {
         self.clean_up().await?;
         forget(self);
@@ -118,10 +156,24 @@ impl DirLock<'_> {
         }
         Ok(())
     }
+
+    fn clean_up_sync(&self) -> Result<(), Error> {
+        if let Err(e) = std::fs::remove_file(self.0.join("pid")) {
+            if e.kind() != io::ErrorKind::NotFound {
+                return Err(e.at(self.0.join("pid")));
+            }
+        }
+        if let Err(e) = std::fs::remove_dir(self.0) {
+            if e.kind() != io::ErrorKind::NotFound {
+                return Err(e.at(self.0));
+            }
+        }
+        Ok(())
+    }
 }
 
 impl Drop for DirLock<'_> {
     fn drop(&mut self) {
-        block_on(self.clean_up()).expect("failed to clean up dir lock");
+        self.clean_up_sync().expect("failed to clean up dir lock");
     }
 }
